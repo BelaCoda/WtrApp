dca26071124a977f2e7db9db087654aa9cec2901
BelaC0da <110172758+BelaCoda@users.noreply.github.com>
2022-08-04 13:28:47 +0300

    Add files via upload

---
+++ all_styles.js
@@ -0,0 +1,147 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+
+export const ALLstyles = StyleSheet.create({
+   
+  
+  fortext: {
+    fontSize:20,
+    textAlign:'center',
+    paddingTop: 40,
+    paddingBottom: 15,
+    fontWeight:'bold',
+    color:'white'
+
+  },
+    
+  main:{
+    flex:1,
+    backgroundColor: 'black'
+
+  },
+
+  forimage:{
+    margin:15,
+    alignItems:'center',
+    borderRadius: 20,
+    
+    backgroundColor:'#2d3036'
+    
+    
+  },
+  
+  for3view:{
+    width: '90%',
+    height: 155,
+    backgroundColor:'black',
+    margin:15,
+    borderRadius: 20,
+    padding: 10,
+  },
+  
+  fortext2:{
+    color:'#444447',
+  },
+  fortext2p:{
+    color:'#444447',
+    paddingLeft:10,
+  },
+  
+  fortext2ptp:{
+    color:'#444447',
+    paddingTop:30
+  },
+
+  fortext2pl:{
+    color:'#444447',
+    paddingLeft:20,
+  },
+
+  fortext2pll:{
+    color:'#444447',
+    paddingLeft:30,
+  },
+
+  fortext2pdtp17pdlf20:{
+    color:'#444447',
+    paddingTop:17,
+    paddingLeft:20
+  },
+  
+  main2:{
+    flex:1,
+    backgroundColor: 'black',
+    justifyContent:'center',
+    alignItems :'center',
+  },
+
+  main2text:{
+    color :'black',
+    fontSize:18,
+    textAlign:'center',
+  },
+
+  foricon:  {
+    alignItems:'center',
+    paddingLeft:4,
+    justifyContent:'center',
+  },
+
+  foricon3:  {
+    color:'white'
+  },
+
+  halfcont:{
+    alignItems:'center',
+    flex: 1,
+    justifyContent:'center'
+  },
+
+  fortepm:{
+    color:'white',
+    alignItems:'center',
+    paddingTop:10,
+    justifyContent:'center',
+    fontWeight:'bold',
+    fontSize:25
+  },
+
+  halfcont2:{
+    alignItems:'center',
+    flex: 1,
+    justifyContent:'center',
+    backgroundColor:'#2d3036',
+    borderRadius: 20,
+    marginBottom:25,
+    width:'85%'
+  },
+
+  halfcont3:{
+    alignItems:'center',
+    flex: 1,
+    justifyContent:'center',
+    backgroundColor:'#786c6b',
+    borderRadius: 15,
+    margin:15,
+    width:'90%'
+  },
+
+  halfcont31:{
+    alignItems:'center',
+    flex: 1,
+    justifyContent:'center',
+    backgroundColor:'#ada4a3',
+    borderRadius: 15,
+    margin:15,
+    width:'90%'
+  },
+
+  main2text1:{
+    color :'black',
+    fontWeight:'bold',
+    fontSize:18,
+    textAlign:'center',
+  },
+
+
+  });
